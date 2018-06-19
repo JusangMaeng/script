e1c58642f06b655b74630b3dbe00080b3e75646e
JS.Maeng <maeng@telechips.com>
2018-06-19 11:35:16 +0900

    for_loop_remove.sh : add exception directory when remove.
    Change-Id: I1945882bb0faeb811727587c3899da250e41e236

--- sample_script/for_loop_remove.sh
+++ sample_script/for_loop_remove.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 
-list=(3dmali bluez-tools commonapi gst-plugins-itapi gstreamer_mediaplay iptv kernel prebuilt  system-api tccbox tcc_launcher tclog tcresource tdd-prebuilt teleplayer tms-prebuilt u-boot)
+list=(3dmali bluez-tools commonapi gst-plugins-itapi gstreamer_mediaplay iptv kernel prebuilt  system-api tccbox tcc_launcher tclog tcresource tdd-prebuilt teleplayer tms-prebuilt u-boot tools docs)
 
 for i in "${list[@]}"
 do
-	cd $i
-	rm -rf *
+	cd $i/git
+	rm -rf * ./!\(.git\)
 	cd -
 done
