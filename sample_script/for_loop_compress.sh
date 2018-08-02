788e638581f124b93115769baf3c47ef41dd11dc
JS.Maeng <maeng@telechips.com>
2018-08-02 11:03:57 +0900

    for_loop_compress : modify list in a row
    Change-Id: I98a0ea0c7de1e9241570b5a07cf270a152746aa1

--- sample_script/for_loop_compress.sh
+++ sample_script/for_loop_compress.sh
@@ -1,6 +1,29 @@
 #!/bin/sh
 
-list=(3dmali bluez-tools commonapi gst-plugins-itapi gstreamer_mediaplay iptv kernel prebuilt  system-api tccbox tcc_launcher tclog tcresource tdd-prebuilt teleplayer tms-prebuilt u-boot tools docs simpleplayer)
+list=(\
+	3dmali \
+	bluez-tools \
+	commonapi \
+	gst-plugins-itapi \
+	gstreamer_mediaplay \
+	iptv \
+	kernel \
+	prebuilt \
+	system-api \
+	tccbox \
+	tcc_launcher \
+	tclog tcresource \
+	tdd-prebuilt \
+	teleplayer \
+	tms-prebuilt \
+	u-boot \
+	tools \
+	docs \
+	simpleplayer \
+	jp-factory \
+	mediafx \
+	tc_bluetooth \
+	)
 
 for i in "${list[@]}"
 do
