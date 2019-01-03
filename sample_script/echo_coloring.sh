4fa40d41638ea97dede51813408660df92573b8c
JS.Maeng <maeng@telechips.com>
2019-01-03 18:08:42 +0900

    Add coloring example
    Change-Id: I1451860e1ed75aecbba8c4eff4feaf64103fed52

---
+++ sample_script/echo_coloring.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+NC='\033[0m' # No Color
+
+BLACK='\033[0;30m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+WHITE='\033[0;37m'
+
+echo -e "${BLACK}hello BLACK${NC}"
+echo -e "${RED}hello RED${NC}"
+echo -e "${GREEN}hello GREEN${NC}"
+echo -e "${YELLOW}hello YELLOW${NC}"
+echo -e "${BLUE}hello BLUE${NC}"
+echo -e "${PURPLE}hello PURPLE${NC}"
+echo -e "${CYAN}hello CYAN${NC}"
+echo -e "${WHITE}hello WHITE${NC}"
